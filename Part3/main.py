c867324c3f5ab852e03d1bb6d3eb5751950e754a
januszekm3 <jmirosla@student.agh.edu.pl>
2014-12-16 19:22:34 +0100

    add .gitignore

--- Part3/main.py
+++ Part3/main.py
@@ -8,11 +8,11 @@ from Interpreter import Interpreter
 import os
 
 if __name__ == '__main__':
-    os.sys.setrecursionlimit(500)
+    os.sys.setrecursionlimit(5000)
 
     try:
-        #filename = sys.argv[1] if len(sys.argv) > 1 else "acceptance_test.py"
-        filename = sys.argv[1] if len(sys.argv) > 1 else "..\\tests\\fact.in"
+        filename = sys.argv[1] if len(sys.argv) > 1 else "acceptance_test.py"
+        #filename = sys.argv[1] if len(sys.argv) > 1 else "..\\tests\\fact.in"
         #filename = sys.argv[1] if len(sys.argv) > 1 else "..\\tests\\fib.in"
         #filename = sys.argv[1] if len(sys.argv) > 1 else "..\\tests\\funcdef.in"
         #filename = sys.argv[1] if len(sys.argv) > 1 else "..\\tests\\gcd.in"
