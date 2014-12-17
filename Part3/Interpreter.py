9f220f23a8088222339b0685a0c02ef682d572d7
januszekm3 <jmirosla@student.agh.edu.pl>
2014-12-17 16:29:01 +0100

    Fix bugs

--- Part3/Interpreter.py
+++ Part3/Interpreter.py
@@ -110,7 +110,7 @@ class Interpreter(object):
     def visit(self, node, scope=0):
         for i in node.list:
             i.accept(self)
-            
+
     @when(AST.Instruction)
     def visit(self, node, scope=0):
         pass
@@ -145,7 +145,6 @@ class Interpreter(object):
     @when(AST.LabeledInstr)
     def visit(self, node, scope=0):
         return node.instruction.accept(self, scope)
-
 
     @when(AST.ChoiceInstruction)
     def visit(self, node, scope=0):
